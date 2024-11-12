0600d3b998e1f0be13a9e0ad41acecee1a5da6e8
Aidan <expertsummer2002@gmail.com>
2024-11-11 23:20:32 -0600

    Create empty Sound/Channel/ChannelGroup/DSP classes

--- addons/FMOD/native/src/api/core_api.cpp
+++ addons/FMOD/native/src/api/core_api.cpp
@@ -11,3 +11,43 @@ void CoreSystem::set_instance(FMOD::System* core_system)
 {
     this->core_system = core_system;
 }
+
+void Sound::_bind_methods()
+{
+
+}
+
+void Sound::set_instance(FMOD::Sound* sound)
+{
+    this->sound = sound;
+}
+
+void Channel::_bind_methods()
+{
+
+}
+
+void Channel::set_instance(FMOD::Channel* channel)
+{
+    this->channel = channel;
+}
+
+void ChannelGroup::_bind_methods()
+{
+
+}
+
+void ChannelGroup::set_instance(FMOD::ChannelGroup* channel_group)
+{
+    this->channel_group = channel_group;
+}
+
+void DSP::_bind_methods()
+{
+
+}
+
+void DSP::set_instance(FMOD::DSP* dsp)
+{
+    this->dsp = dsp;
+}
--- addons/FMOD/native/src/api/core_api.h
+++ addons/FMOD/native/src/api/core_api.h
@@ -6,9 +6,16 @@
 namespace CoreApi
 {
 
+class Sound;
+// note(aidan): i don't know c++ well enough to figure out how to port this ChannelControl abstraction over naturally
+//              so just be wary that you might have to fix some bugs twice.
+class Channel;
+class ChannelGroup;
+class DSP;
+
 class CoreSystem : public RefCounted
 {
-    GDCLASS(CoreSystem, RefCounted)
+    GDCLASS(CoreSystem, RefCounted);
 
 protected:
     static void _bind_methods();
@@ -18,6 +25,62 @@ private:
 
 public:
     void set_instance(FMOD::System* core_system);
+};
+
+class Sound
+{
+    GDCLASS(Sound, RefCounted);
+
+protected:
+    static void _bind_methods();
+
+private:
+    FMOD::Sound* sound = nullptr;
+
+public:
+    void set_instance(FMOD::Sound* sound);
+};
+
+class Channel
+{
+    GDCLASS(Channel, RefCounted);
+
+protected:
+    static void _bind_methods();
+
+private:
+    FMOD::Channel* channel = nullptr;
+
+public:
+    void set_instance(FMOD::Channel* channel);
+};
+
+class ChannelGroup
+{
+    GDCLASS(ChannelGroup, RefCounted);
+
+protected:
+    static void _bind_methods();
+
+private:
+    FMOD::ChannelGroup* channel_group = nullptr;
+
+public:
+    void set_instance(FMOD::ChannelGroup* channel_group);
+};
+
+class DSP
+{
+    GDCLASS(DSP, RefCounted);
+
+protected:
+    static void _bind_methods();
+
+private:
+    FMOD::DSP* dsp = nullptr;
+
+public:
+    void set_instance(FMOD::DSP* dsp);
 };
 
 }
